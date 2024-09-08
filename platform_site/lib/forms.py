19c7f24fd4c0dc8b5f947ccbd1bf84e260ccff4f
Sharjeel Mustafa <sharjeeliv@gmail.com>
2024-09-07 23:55:20 -0400

    removed uneeded import

--- platform_site/lib/forms.py
+++ platform_site/lib/forms.py
@@ -1,5 +1,4 @@
 # Third-party Imports
-from os import link
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, SelectField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError, Optional
